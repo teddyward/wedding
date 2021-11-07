788dd41199a8e1fb38e0303e26c6c568af47fc22
Teddy Ward <teddy@kevalaanalytics.com>
2021-11-07 10:30:06 -0500

    add instructions to Durham page

--- src/routes/durham.svelte
+++ src/routes/durham.svelte
@@ -1,5 +1,6 @@
-<div class="page-title">
-    <div>What to do in Durham</div>
+<div class="page-title durham-page-title">
+    <div>Things to do in Durham</div>
+    <div class="page-subtitle">Zoom in for descriptions</div>
 </div>
 <div id="map"></div>
 
@@ -9,7 +10,7 @@
     onMount(() => {
         mapboxgl.accessToken = 'pk.eyJ1IjoidGhyb3dhd2F5ODY3NTMwOSIsImEiOiJja2Q3bmZ4c2ExZWw0MnlvMmZ2OWR1MHE5In0.lt1pIT7P5njAWbfnucdQmQ';
 
-        var map = new mapboxgl.Map({
+        const map = new mapboxgl.Map({
             container: 'map', // container id
             style: 'mapbox://styles/throwaway8675309/cku1unoy72a8318o6irmyf83v', // style URL
             center: [-78.90581, 36.00548], // starting position [lng, lat]
@@ -22,6 +23,15 @@
     #map {
         height: 100%;
         margin-top: 20px;
-        width: 100%;
+        width: 90%;
+    }
+    .durham-page-title {
+        align-items: center;
+        flex-direction: column;
+    }
+
+    .page-subtitle {
+        font-size: 12px;
+        font-style: italic;
     }
 </style>
