7ef127fe581afa359d7f9b978935888c7ab062c8
Teddy Ward <teddy@kevalaanalytics.com>
2022-05-21 15:42:44 -0400

    update map a bit

--- src/routes/durham.svelte
+++ src/routes/durham.svelte
@@ -3,6 +3,11 @@
     <div class="page-subtitle">Zoom in for descriptions</div>
 </div>
 <div id="map"></div>
+<div class="external-guides">
+    <div>In addition, you can check out the following resources:</div>
+    <div><a href="https://www.vogue.com/article/durham-travel-guide-north-carolinas-hippest-city"><i>Why Durham is North Carolina's Hippest City</i></a> in Vogue</div>
+    <div><a href="https://www.alltrails.com/us/north-carolina/durham">AllTrails guide to Durham</a></div>
+</div>
 
 <script>
     import { onMount } from 'svelte';
@@ -13,16 +18,19 @@
         const map = new mapboxgl.Map({
             container: 'map', // container id
             style: 'mapbox://styles/throwaway8675309/cku1unoy72a8318o6irmyf83v', // style URL
-            center: [-78.90581, 36.00548], // starting position [lng, lat]
-            zoom: 12.32 // starting zoom
+            center: [-78.91618516143913, 36.020195587756106], // starting position [lng, lat]
+            zoom: 11.09 // starting zoom
         });
+        map.on('zoomend', () => alert(map.getZoom()))
+        setTimeout(() => map.resize(), 1000)
     });
 </script>
 
 <style>
     #map {
-        height: 100%;
+        height: 75%;
         margin-top: 20px;
+        margin-bottom: 20px;
         min-width: 90%;
         width: 90%;
     }
@@ -34,5 +42,13 @@
     .page-subtitle {
         font-size: 12px;
         font-style: italic;
+    }
+
+    .external-guides {
+        display: flex;
+        height: 20%;
+        flex-direction: column;
+        justify-content: space-around;
+        align-items: center;
     }
 </style>
