5be4c8dbbae8cf33d1cc562521086f8931803194
Teddy Ward <teddy@kevalaanalytics.com>
2022-05-21 14:27:37 -0400

    another update

--- src/routes/schedule.svelte
+++ src/routes/schedule.svelte
@@ -12,11 +12,11 @@
             <i>Friday, 6:35PM</i>
         </div>
         <div class="section-body">
-            Durham Bulls Athletic Park<br>
-            For anyone who is in town early, we have reserved boxes
-            at the minor league stadium for the Durham Bulls - "famously" profiled in the Kevin Costner
-            and Susan Sarandon movie <i>Bull Durham</i> - as they play the Charlotte Knights.<br>
-            Text Teddy (847 346 9673) if you'll be around, and he'll send you a ticket!
+            Durham Bulls Athletic Park, 409 Blackwell Street, Durham<br>
+            For anyone who is in town early, we have reserved a section
+            for AAA champions Durham Bulls - "famously" profiled in the Kevin Costner and Susan Sarandon movie <i>Bull Durham</i> - as they play the Charlotte Knights.<br>
+            Text Teddy (847 346 9673) if you'll be around, and he'll send you a ticket! You'll have to create a Durham Bulls account to access it, unfortunately, but then you can screenshot it or add it to an Apple wallet.<br>
+            The ballpark is within walking distance of most places people are staying, but there's also parking at the American Tobacco Campus or the Corcoran Street Garage.
         </div>
     </div>
     <div class="timeline-section" data-date="5.28.22">
@@ -25,15 +25,10 @@
             <i>Saturday, 8PM</i>
         </div>
         <div class="section-body">
-            Vin Rouge<br>
-            2010 Hillsborough Rd.<br>
+            Vin Rouge, 2010 Hillsborough Rd, Durham<br>
             We can't wait to see you there!<br>
             If you're looking for things to do before or after, check out the "Durham" tab.<br>
-            We understand that this is somewhat unusual to start
-            a wedding on the Saturday before a holiday (Memorial Day),
-            but to help our many guests who have to travel,
-            we've scheduled this event over a long weekend.<br>
-            There's a large parking lot nearby on Hillsborough Road, or you can drive/Uber/Lyft.            
+            There's a large parking lot nearby on Hillsborough Road, or you can Uber/Lyft. Please don't drink and drive!
         </div>
     </div>
     <div class="timeline-section" data-date="5.29.22">
@@ -42,14 +37,14 @@
             <i><strong>Sunday</strong>, 6:30PM</i>
         </div>
         <div class="section-body">
-            Museum of Life and Science, Durham.<br>
+            Museum of Life and Science, 433 W Murray Ave, Durham.<br>
             A shuttle will pick up from the 21c Hotel at 6pm and 6:20pm, or you can drive/Uber/Lyft.<br>
             Cocktails, hors d'oeuvres, and our vow exchange will be at 6:30PM in the butterfly house courtyard.<br>
             The butterfly house will be open to guests from 7-7:45PM.<br>
             Dinner under the white top will be served at 8PM, catered by Virginia and Co. 
             Music and dancing in the garden, to follow.<br>
             A shuttle will leave for the 21c Hotel at 10:30pm,
-            and to the after party at Surf Club at 11pm.
+            and another shuttle will leave to the after party at Surf Club at 11pm.
             Afterparty at Surf Club in downtown Durham (open til 2am!)
         </div>
     </div>
@@ -59,9 +54,9 @@
             <i>Monday, 10AM to Noon</i>
         </div>
         <div class="section-body">
-            21c Museum Hotel, 2nd Floor Gallery<br>
-            Before you make the (potentially long) trip back home, we'd love to
-            see you off with some food at the hotel gallery (not the first floor restaurant).<br>
+            21c Museum Hotel, 2nd Floor Gallery, 111 N Corcoran Street, Durham<br>
+            Before you make the trip back home, we'd love to
+            see you off with some food at the hotel gallery (<strong>not</strong> the Counting House restaurant).<br>
             Valet parking is included if you ask the waiter at brunch for a card.
         </div>
     </div>
